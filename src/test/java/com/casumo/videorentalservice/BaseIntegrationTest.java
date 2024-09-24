35048f531c71d963d2710639fc72111d24f749e0
Mykhailo Lipilov <m.lipilov@andersenlab.com>
2024-09-24 15:33:06 +0300

    CSM-3 Added log config and implemented base IT class

--- src/test/java/com/casumo/videorentalservice/BaseIntegrationTest.java
+++ src/test/java/com/casumo/videorentalservice/BaseIntegrationTest.java
@@ -1,12 +1,59 @@
 package com.casumo.videorentalservice;
 
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+import static org.springframework.test.context.jdbc.SqlConfig.TransactionMode.ISOLATED;
+import static org.springframework.test.context.jdbc.SqlMergeMode.MergeMode.MERGE;
+
 import com.casumo.videorentalservice.utils.VideoRentalQueryExecutionListener;
+import java.text.MessageFormat;
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.jdbc.SqlMergeMode;
+import org.testcontainers.containers.PostgreSQLContainer;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ActiveProfiles(profiles = "integration")
+@SqlMergeMode(MERGE)
+@Sql(
+    scripts = {"classpath:db/truncate-tables.sql"},
+    executionPhase = BEFORE_TEST_METHOD,
+    config = @SqlConfig(transactionMode = ISOLATED))
 public class BaseIntegrationTest {
 
+  private static final String POSTGRES_IMAGE = "postgres:latest";
+  private static final String DATASOURCE_URL = "DATASOURCE_URL";
+  private static final String DB_USERNAME = "DB_USERNAME";
+  private static final String DB_PASSWORD = "DB_PASSWORD";
+  private static final String LOCALHOST_URL = "http://localhost:{0}/{1}";
+
+  @LocalServerPort
+  protected int port;
   @Autowired
   protected VideoRentalQueryExecutionListener queryExecutionListener;
+
+  protected static final PostgreSQLContainer<?> POSTGRES_SQL_CONTAINER;
+
+  protected String getLocalhostUrl(@NonNull final String uri) {
+    return MessageFormat.format(LOCALHOST_URL, String.valueOf(port), uri);
+  }
+
+  static {
+    POSTGRES_SQL_CONTAINER = new PostgreSQLContainer<>(POSTGRES_IMAGE);
+    POSTGRES_SQL_CONTAINER.start();
+  }
+
+  @DynamicPropertySource
+  static void registerDynamicProperties(DynamicPropertyRegistry registry) {
+    registry.add(DATASOURCE_URL, POSTGRES_SQL_CONTAINER::getJdbcUrl);
+    registry.add(DB_USERNAME, POSTGRES_SQL_CONTAINER::getUsername);
+    registry.add(DB_PASSWORD, POSTGRES_SQL_CONTAINER::getPassword);
+  }
 }
